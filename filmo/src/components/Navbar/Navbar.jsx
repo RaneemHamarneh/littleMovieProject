0481994d2c0290a6db4f484fc85c5a4cdaca3b51
raneemhamarneh <raneem99hamarneh@gmail.com>
2023-12-05 18:29:16 +0300

    complete layout actors

---
+++ filmo/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+
+const Navbar = () => {
+  return (
+    <div className="bg-gray-800 text-white p-4">
+navBar
+  </div>
+)
+}
+export default Navbar
--- filmo/src/pages/actors/[actorId].jsx
+++ filmo/src/pages/actors/[actorId].jsx
@@ -1,6 +1,6 @@
 import React , {useState, useEffect}from 'react';
 import { useRouter } from 'next/router';
-import { optionsTrending } from './index';
+import { optionsTrending } from '@component/util/API'; 
 
 
 const options = {
@@ -10,6 +10,27 @@ const options = {
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Y2FhZTlhZGM1MjA3Njk5ZDg1Y2ZmN2Y3YTMyMmI2ZSIsInN1YiI6IjY1NjZkNTIxYThiMmNhMDE0ZDUxYTIwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zm4mNzeuZSUcJFa8tVG6ldeD_8OY37zsxEAB8OKUpZI'
   }
 };
+const options1 = {
+  method: 'post',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Y2FhZTlhZGM1MjA3Njk5ZDg1Y2ZmN2Y3YTMyMmI2ZSIsInN1YiI6IjY1NjZkNTIxYThiMmNhMDE0ZDUxYTIwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zm4mNzeuZSUcJFa8tVG6ldeD_8OY37zsxEAB8OKUpZI'
+  }
+};
+
+export  function searchBarFetching (setSearchBar, qq,setIsLoading) {
+  // const router = useRouter();
+  useEffect(()=>{
+  fetch(`https://api.themoviedb.org/3/search/person?query=${qq}&include_adult=false&language=en-US&page=1&api_key=6caae9adc5207699d85cff7f7a322b6e`, options1)
+  .then(response => response.json())
+  .then(response => {
+    console.log(response)
+  setSearchBar(response.results)
+return response.results
+})
+  .catch(err => console.error(err))
+  .finally(() => setIsLoading(false));})}
+
 
 export default function SingleActorPage() {
   const router = useRouter();
@@ -18,6 +39,7 @@ export default function SingleActorPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [showMore, setShowMore] = useState(false);
   const [showMoreMovies, setShowMoreMovies] = useState(false);
+  const [searchBar,setSearchBar] = useState();
 
 
   // Fetching actors by id that cast in this moviesCredit  
@@ -35,7 +57,8 @@ export default function SingleActorPage() {
   console.log(movieCredits);
   // Fetching actors by id
   useEffect(()=>{
-      fetch(`https://api.themoviedb.org/3/person/${router.query.actorId}?language=en-US&api_key=6caae9adc5207699d85cff7f7a322b6e`, optionsTrending)
+      fetch(`https://api.themoviedb.org/3/person/${router.query.actorId}?language=en-US&api_key=6caae9adc5207699d85cff7f7a322b6e`,
+       optionsTrending)
       .then(response => response.json())
       .then(response => {
         console.log(response)
@@ -44,24 +67,29 @@ export default function SingleActorPage() {
       .catch(err => console.error(err))
       .finally(() => setIsLoading(false))
     },[]);
+
     console.log(actor)
+
     function handleShowClick(){
       setShowMore(!showMore);
     }
+
     function handleShowMoviesClick(){
       setShowMoreMovies(!showMoreMovies);
     }
+    
+  searchBarFetching(setSearchBar,actor ,setIsLoading);
       
         return (
-          <div className="bg-gradient-to-l from-black to-indigo-900  min-h-screen text-zinc-400">
-      
+          <div className="bg-gradient-to-r from-black from-40% to-blue-grey  min-h-screen text-red-500/50">
+          
             {isLoading ? (
               <div className="flex justify-center items-center h-screen">
                 <p>Loading actor data...</p>
               </div>
-            ) : actor ? (
+            ) : actor  ? (
               <div className="flex flex-col md:flex-row justify-center items-center md:items-start md:space-x-8 p-8">
-                <div className="bg-gradient-to-l from-indigo-950 to-indigo-900 p-4 rounded-lg shadow-lg mb-4 md:mb-0 flex-shrink-0 ">
+                <div className="bg-gradient-to-l from-white to-indigo-500/10 p-4 rounded-lg shadow-lg mb-4 md:mb-0 flex-shrink-0 ">
                   <img
                     src={`https://image.tmdb.org/t/p/w780/${actor.profile_path}`}
                     alt={actor.name}
@@ -69,28 +97,28 @@ export default function SingleActorPage() {
                   />
                 </div>
                 <div className="flex flex-col space-y-4">
-                  <h1 className="text-4xl text-sky-800 font-bold">{actor.name}</h1>
-                  <p className={actor.gender === 1 ? "text-pink-300" : "text-blue-300"}>
+                  <h1 className="text-4xl text-white font-bold">{actor.name}</h1>
+                  <p className={actor.gender === 1 ? "text-pink-300/80" : "text-blue-300/80 text-lg font-bold "}>
                     {actor.gender === 1 ? 'Female' : 'Male'}
                   </p>
-                  <span className="text-lg font-semibold text-sky-600">
+                  <span className="text-lg font-semibold text-white">
                   Born: {actor.birthday} <br />
                   Popularity: {actor.popularity} %
                 </span>
                   <div className="text-center">
                     <button
-                      className="bg-sky-950 hover:bg-sky-600 text-sky-600 hover:text-sky-900 font-bold py-2 px-4 rounded my-3"
+                      className="bg-black hover:bg-sky-800/80  text-white hover:text-black  font-bold py-2 px-4 rounded my-3"
                       onClick={handleShowClick}
                     >
                       {showMore ? 'Show Less' : 'Show biography'}
                     </button>
                     {showMore && (
                       <div>
-                        <p className="text-gray-600 hover:text-sky-50 mt-2">{actor.biography}</p>
+                        <p className="text-gray-200/75 hover:text-sky-50  md:bg-black  mt-2">{actor.biography}</p>
                       </div>
                     )}
                     <button
-                      className="bg-sky-950 hover:bg-sky-600 text-sky-600 hover:text-sky-900 font-bold py-2 px-4 rounded my-3"
+                      className="bg-black hover:bg-sky-800/80  text-white hover:text-black  font-bold py-2 px-4 rounded my-3"
                       onClick={handleShowMoviesClick}
                     >
                       {showMoreMovies ? 'Show Less' : 'Show Movies'}
@@ -98,15 +126,16 @@ export default function SingleActorPage() {
                     {showMoreMovies && (
                       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
                         {movieCredits && movieCredits.map(movie => (
-                          <div key={movie.id} className="group relative w-40 h-55 overflow-hidden rounded-lg">
+                          <div key={movie.id} className="group relative border-2 border-white shadow-lg  w-70 h-55 overflow-hidden rounded-lg">
                             <img
                               src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`}
                               alt={movie.original_title}
-                              className="w-auto h-auto object-cover transition duration-300 ease-in-out"
+                              className="w-auto h-auto object-cover  md:bg-sky-700/50 transition duration-300 ease-in-out"
                             />
-                            <h2 className="text-center text-sky-800 text-lg mt-2">{movie.original_title} <br/>
-                            Release Date:<br/>{movie.release_date}</h2>
-                            <div className="absolute inset-0 bg-white bg-opacity-0 group-hover:bg-opacity-10"></div>
+                            <h3 className="text-center text-white font-bold text-lg mt-2">
+                            {movie.original_title} <br/>
+                            popularity:{movie.popularity}
+                            </h3>
                           </div>
                         ))}
                       </div>
