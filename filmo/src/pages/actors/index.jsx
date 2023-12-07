44cfa1075c98f99e37bd2eee9b5dc896c57c33a8
raneemhamarneh <raneem99hamarneh@gmail.com>
2023-12-07 03:59:05 +0300

    copy final stage with little edit on actors page

--- filmo/src/pages/actors/index.jsx
+++ filmo/src/pages/actors/index.jsx
@@ -45,18 +45,6 @@ export default function Index() {
   if (isLoading) return <p>Loading...</p>
   if (!actors) return <p>No profile data</p>
 
-  const handleActorsNameSubmit = (event) => {
-    event.preventDefault()
-  }
-
-  const handleSelectChange = (event) => {
-    const actorId = event.target.value
-    setSelectedActorId(actorId)
-
-    if (actorId) {
-      router.push(`/actors/${actorId}`)
-    }
-  }
 
   return (
     <div className="bg-gradient-to-l from-black to-indigo-900  min-h-screen text-zinc-400">
@@ -64,49 +52,21 @@ export default function Index() {
         <h1 className="text-center animated-gradient text-5xl font-bold">
           ⭐FILMO⭐
         </h1>
-
-        <label
-          htmlFor="search-bar"
-          className="mb-2 text-gray-700 text-sm font-semibold"
-        >
-          lets search here:{" "}
-        </label>
-        <form
-          onSubmit={handleActorsNameSubmit}
-          id="search-bar"
-          className="flex"
-        >
-          <select
-            value={actors}
-            onChange={handleSelectChange}
-            className="animated-gradient text-1xl
-                 font-bold bg-sky-800 text-black
-                 rounded-lg p-2 border focus:outline-none focus:ring "
-            placeholder="options"
-          >
-            {actors &&
-              actors.map((user) => (
-                <option key={user.id} value={user.id}>
-                  <Link href={`/actors/${user.id}`} key={user.id}>
-                    {user.name}
-                  </Link>
-                </option>
-              ))}
-          </select>
-        </form>
-      </div>
-      <h1 className=" w-full flex justify-center pt-4 text-3xl font-bold bg-black rounded-full text-sky-700 px-6 py-3">
+      <h1 className=" w-full flex justify-center pt-4 text-3xl font-bold 
+      bg-black rounded-full text-sky-700 px-6 py-3">
         Actors
-      </h1>
-      <div className="flex justify-center bg-gradient-to-l from-black to-indigo-900  shadow-lg rounded-lg p-6 w-full">
+      </h1></div>
+      <div className="flex justify-center bg-gradient-to-l from-black to-indigo-900 
+       shadow-lg rounded-lg p-6 w-full">
         <div className="grid grid-cols-4 gap-4 max-w-6xl w-full">
           {actors &&
             actors.map((user) => (
               <Link href={`/actors/${user.id}`} key={user.id}>
                 <div
                   key={user.id}
-                  className="flex flex-col items-start bg-black hover:bg-sky-800 text-white font-bold py-2 px-4 rounded transition duration-200 ease-in-out shadow-lg"
-                >
+                  className="flex flex-col items-start bg-black hover:bg-sky-800
+                   text-white font-bold py-2 px-4 rounded transition duration-200 
+                   ease-in-out shadow-lg">
                   <img
                     src={`https://image.tmdb.org/t/p/w780/${user.profile_path}`}
                     width={500}
